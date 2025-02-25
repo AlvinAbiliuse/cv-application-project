8122274e90cc3bcfa3956c27e4f79fc2ea52cf8d
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-25 05:50:29 +0400

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import { Personal } from "./Personal.jsx";
 /*
-import {Personal} from "./Personal.jsx";
+
 import {Education} from "./Education.jsx";
 import {Experience} from "./Experience.jsx";
 import {FullCV} from "./FullCV.jsx";
@@ -8,15 +9,16 @@ import {FullCV} from "./FullCV.jsx";
 
 function App() {
 	let [state, setState] = useState({
-		personal: {},
-		education: {},
-		experience: {},
+		personal: { show: true },
+		education: { show: true },
+		experience: { show: true },
 	});
 
 	return (
 		<>
 			<div className="left">Hello</div>
 			<div className="right">Bye</div>
+			<Personal state={state} setState={setState} />
 		</>
 	);
 }
--- src/Personal.jsx
+++ src/Personal.jsx
@@ -1,26 +1,43 @@
-function DropBtn({ current, s, setS}) {
-	if (s[current]["show"]) {
-		console.log(s);
-		let c = s;
-		c[current]["show"] =  false;
-		setS(c);
-		console.log(s);
-		return (<button>-</button>)
+function Change({ state, setState }) {
+	console.log(state);
+	if (state["personal"]["show"]) {
+		console.log(state);
+		let c = state;
+		c["personal"]["show"] = false;
+		setState(c);
+		console.log(state);
+		return <button>+</button>;
 	} else {
-		console.log(s);
-		let c = s;
-		c[current]["show"] = true;
-		setS(c);
-		console.log(s);
-		return (<button>+</button>)
+		console.log(state);
+		let c = state;
+		c["personal"]["show"] = true;
+		setState(c);
+		console.log(state);
+		return <button>-</button>;
 	}
 }
 
-
-export default function Personal([state, setState]) {
+export function Personal({ state, setState }) {
 	return (
 		<div className="card">
-		<div className="cardHeader">
-			<h2>Personal Details</h2>
-			<DropBtn current={personal} s={state}, setS={setState} />
-
+			<div className="cardHeader">
+				<h2>Personal Details</h2>
+				<button> + </button>
+			</div>
+			<div className="cardContents">
+				<div>
+					<label htmlFor="name">Full Name</label>
+					<input type="text" id="name"></input>
+				</div>
+				<div>
+					<label htmlFor="email">Email</label>
+					<input type="email" id="email"></input>
+				</div>
+				<div>
+					<label htmlFor="number">Phone Number</label>
+					<input type="tel" id="name"></input>
+				</div>
+			</div>
+		</div>
+	);
+}
