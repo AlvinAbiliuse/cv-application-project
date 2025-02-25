3a2368bfb066c041971d10967c39f1d1735c2cee
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-25 04:35:06 +0400

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import { useState } from "react";
 
+function App() {
+	let [state, setState] = useState({
+		personal: {},
+		education: {},
+		experience: {},
+	});
+
+	return (
+		<>
+			<div class="left">Hello</div>
+
+			<div class="right">Bye</div>
+		</>
+	);
+}
+
+export default App;
