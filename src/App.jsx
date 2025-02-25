a670b57bb6bcfb167341f8d2d64a66f89d74eba2
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-25 04:50:02 +0400

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
+import First from "./First.jsx";
 
 function App() {
+	/*
 	let [state, setState] = useState({
 		personal: {},
 		education: {},
 		experience: {},
 	});
+	*/
+	const [s, setS] = useState(0);
 
 	return (
 		<>
 			<div class="left">Hello</div>
-
+			<First s={s} setS={setS} />
 			<div class="right">Bye</div>
 		</>
 	);
