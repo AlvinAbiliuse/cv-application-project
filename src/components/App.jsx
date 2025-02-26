187be1501b28ce3e667b6143dd049e9a639e1b7c
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-26 16:29:00 +0400

    progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,8 +11,8 @@ import {FullCV} from "./FullCV.jsx";
 
 const baseState = {
 	personal: { show: true, name: "", email: "", tel: "" },
-	education: { show: true, cards: [] },
-	experience: { show: true, cards: [] },
+	education: { show: true, cards: [{ name: "", degree: "" }] },
+	experience: { show: true, cards: [{ name: "", duration: "", position: "" }] },
 };
 
 const example = {
@@ -22,9 +22,26 @@ const example = {
 		email: "jarodthetarotreader@tarot.com",
 		tel: "900-500-100",
 	},
-	education: { show: true },
+	education: {
+		show: true,
+		cards: [
+			{ name: "u of u", degree: "bachelors of bachelorhood" },
+			{ name: "university of earth", degree: "masters of bachelorhood" },
+		],
+	},
 	experience: { show: true },
 };
+
+function Education({ state }) {
+	state.education.cards.forEach(() => {
+		return (
+			<div className="card">
+				<h2>Education: {state.education.cards.name}</h2>
+				<p>Degree: {state.education.cards.degree}</p>
+			</div>
+		);
+	});
+}
 
 function App() {
 	let [state, setState] = useState(JSON.parse(JSON.stringify(example)));
@@ -59,6 +76,7 @@ function App() {
 				<p>
 					Phone Number: <span>{state.personal.tel}</span>
 				</p>
+				<Education state={state} />
 			</div>
 		</>
 	);
