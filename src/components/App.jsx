65540268c715eee2387328454457d73b2c2765b7
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-26 15:42:15 +0400

    App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,31 +8,56 @@ import {Experience} from "./Experience.jsx";
 import {FullCV} from "./FullCV.jsx";
 */
 
-let baseState = {
-	personal: { show: true },
+const baseState = {
+	personal: { show: true, name: "", email: "", tel: "" },
+	education: { show: true, cards: [] },
+	experience: { show: true, cards: [] },
+};
+
+const example = {
+	personal: {
+		show: true,
+		name: "Jarod Tarot",
+		email: "jarodthetarotreader@tarot.com",
+		tel: "900-500-100",
+	},
 	education: { show: true },
 	experience: { show: true },
 };
 
 function App() {
-	let [state, setState] = useState(baseState);
-	console.log(state);
+	let [state, setState] = useState(JSON.parse(JSON.stringify(example)));
 
 	return (
 		<>
 			<div className="left">
 				<button
-					className="btn reset"
-					onClick={() => setState((s) => (s = baseState))}
+					className="btn reset-btn"
+					onClick={() => {
+						setState(JSON.parse(JSON.stringify(baseState)));
+					}}
 				>
 					Reset
+				</button>
+
+				<button
+					className="btn example-btn"
+					onClick={() => setState(JSON.parse(JSON.stringify(example)))}
+				>
+					Load Example
 				</button>
 				<Personal state={state} setState={setState} />
 			</div>
 			<div className="right">
-				<p>Name: {state.personal.name ?? ""}</p>
-				<p>Email: {state.personal.email}</p>
-				<p>Phone Number: {state.personal.tel}</p>
+				<p>
+					Name: <span>{state.personal.name}</span>
+				</p>
+				<p>
+					Email: <span>{state.personal.email}</span>
+				</p>
+				<p>
+					Phone Number: <span>{state.personal.tel}</span>
+				</p>
 			</div>
 		</>
 	);
