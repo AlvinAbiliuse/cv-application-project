7e00d06bf898ca2818cc57cbb1cbab51c0fce8d9
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-27 02:49:24 +0400

    moved baseState and example objects into seperate file and imported them as modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Personal } from "./Personal.jsx";
+import { baseState, example } from "./baseData.js";
 import "../styles/index.css";
 
 import "../styles/leftStyles.css";
@@ -8,45 +9,6 @@ import {Education} from "./Education.jsx";
 import {Experience} from "./Experience.jsx";
 import {FullCV} from "./FullCV.jsx";
 */
-
-const baseState = {
-	personal: { show: true, name: "", email: "", tel: "" },
-	education: { show: true, cards: [{ name: "", degree: "" }] },
-	experience: { show: true, cards: [{ name: "", duration: "", position: "" }] },
-};
-
-const example = {
-	personal: {
-		show: true,
-		name: "Jarod Tarot",
-		email: "jarodthetarotreader@tarot.com",
-		tel: "900-500-100",
-	},
-	education: {
-		show: true,
-		cards: [
-			{ name: "u of u", degree: "bachelors of bachelorhood" },
-			{ name: "university of earth", degree: "masters of bachelorhood" },
-		],
-	},
-	experience: {
-		show: true,
-		cards: [
-			{ name: "the tarot company", duration: "1 year", position: "intern" },
-			{
-				name: "the tarot company",
-				duration: "2 years",
-				position: "full time employee",
-			},
-			{
-				name: "the magic factory",
-				duration: "3 years",
-				position: "regional manager",
-			},
-			{ name: "Jarod Tarot evil Inc.", duration: "5 years", position: "CEO" },
-		],
-	},
-};
 
 function Education({ state }) {
 	let cards = state.education.cards;
