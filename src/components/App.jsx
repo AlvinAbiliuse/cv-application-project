8a491a1e7a657856f1fc1e32e57529efa45f42b0
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-28 03:49:42 +0400

    Progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,12 @@ import { Personal } from "./Personal.jsx";
 import { baseState, example } from "./baseData.js";
 import { FullCV } from "./FullCV.jsx";
 import { Education } from "./Education.jsx";
+import { Experience } from "./Experience.jsx";
 import "../styles/index.css";
 
 import "../styles/leftStyles.css";
 /*
-import {Experience} from "./Experience.jsx";
-*/
+ */
 
 function App() {
 	let [state, setState] = useState(JSON.parse(JSON.stringify(example)));
@@ -33,6 +33,7 @@ function App() {
 				</button>
 				<Personal state={state} setState={setState} />
 				<Education state={state} setState={setState} />
+				<Experience state={state} setState={setState} />
 			</div>
 			<div className="right">
 				<FullCV state={state} />
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,8 +1,13 @@
 function Cards({ state }) {
 	let card = state["experience"]["cards"];
-	return card.map((e) => {
+	return card.map((e, index) => {
 		console.log(e);
-		return <p>Hello</p>;
+		return (
+			<div key={e["id"]} className="card">
+				<label htmlFor={"name-" + index}>Company: </label>
+				<input type="text" id={"name-" + index} value={e.name} />
+			</div>
+		);
 	});
 }
 
