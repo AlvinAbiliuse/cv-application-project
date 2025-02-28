59ff84d0ae83b80dce2f9937dfd8740a080637e8
AlvinAbiluse <alvinabiliuse@gmail.com>
2025-02-28 04:01:35 +0400

    Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,41 @@
-function Cards({ state }) {
+function updateState(e, state, setState) {
+	let tempState = JSON.parse(JSON.stringify(state));
+	let tempData = e.target.id.split("-");
+	tempState["education"]["cards"][tempData[1]][tempData[0]] = e.target.value;
+	setState(tempState);
+}
+
+function Cards({ state, setState }) {
 	let card = state["experience"]["cards"];
 	return card.map((e, index) => {
 		console.log(e);
 		return (
 			<div key={e["id"]} className="card">
-				<label htmlFor={"name-" + index}>Company: </label>
-				<input type="text" id={"name-" + index} value={e.name} />
+				<div>
+					<div>
+						<label htmlFor={"name-" + index}>Company: </label>
+						<input type="text" id={"name-" + index} value={e.name} />
+					</div>
+					<div>
+						<label htmlFor={"duration-" + index}>Duration: </label>
+						<input
+							onChange={(e) => updateState(e, state, setState)}
+							type="text"
+							id={"duration-" + index}
+							value={e.duration}
+						/>
+					</div>
+					<div>
+						<label htmlFor={"position-" + index}>Position: </label>
+						<input
+							onChange={(e) => updateState(e, state, setState)}
+							type="text"
+							id={"position-" + index}
+							value={e.position}
+						/>
+					</div>
+				</div>
+				<button className="remove">Remove</button>
 			</div>
 		);
 	});
